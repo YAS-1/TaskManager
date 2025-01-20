b6b19f66eb8b2ce178265c141998d4aeb3cf8444
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2025-01-20 10:07:01 +0300

    taskManager

--- frontend/src/utils/TaskCard.jsx
+++ frontend/src/utils/TaskCard.jsx
@@ -11,12 +11,12 @@ const TaskCard = ({task}) => {
                 transition={"all 0.3s"}
                 bg={"blackAlpha.800"}
                 _hover={{ transform: "translateY(-5px)", shadow: "xl" }}>
-                    <Box p={4} minH={"250px"}  minW={"150px"}bgGradient="to-br" gradientFrom={"purple.400"}gradientVia={"green.400"}gradientTo={"orange.400"}>
+                    <Box p={4} minH={"250px"}  minW={"150px"}>
                         <Text fontSize={"xl"} fontFamily={"cursive"}>{task.taskDescription}</Text>
                     </Box >
 
                     {task.category==="Academic" && 
-                            <Box minH={"100px"} padding={"20px"} bgColor={"red.400"}>
+                            <Box minH={"100px"} padding={"20px"} bgColor={"red.600"}>
                             <Text fontWeight={"bold"} fontSize={"2xl"} md={4} fontFamily={"cursive"}> #{task.taskName} </Text>
                             <Box>
                                 <Text fontSize={"lg"} fontFamily={"cursive"}>Category: {task.category}</Text>
@@ -26,7 +26,7 @@ const TaskCard = ({task}) => {
                             </Box>
                     }
                     {task.category==="Personal" && 
-                            <Box minH={"100px"} padding={"20px"} bgColor={"blue.400"}>
+                            <Box minH={"100px"} padding={"20px"} bgColor={"blue.600"}>
                             <Text fontWeight={"bold"} fontSize={"2xl"} md={4} fontFamily={"cursive"}> #{task.taskName} </Text>
                             <Box>
                                 <Text fontSize={"lg"} fontFamily={"cursive"}>Category: {task.category}</Text>
